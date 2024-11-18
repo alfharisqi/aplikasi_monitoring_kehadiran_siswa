d1761605e8319875325b64b5dae0b44cef9a14ba
alfhaberdasi <risqiwicaksonoalfha.com>
2024-11-18 09:28:57 +0700

    update background logo kedua

--- lib/screens/riwayat_kehadiran.dart
+++ lib/screens/riwayat_kehadiran.dart
@@ -10,15 +10,30 @@ class HistoryScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(title: Text('Riwayat Kehadiran Mahasiswa')),
-      body: ListView.builder(
-        itemCount: attendanceHistory.length,
-        itemBuilder: (ctx, index) {
-          final record = attendanceHistory[index];
-          return ListTile(
-            title: Text(DateFormat('dd MMM yyyy').format(record['date'])),
-            subtitle: Text('Hadir: ${record['present']}, Tidak Hadir: ${record['absent']}'),
-          );
-        },
+      body: Stack(
+        children: [
+          // Background logo
+          Positioned.fill(
+            child: Opacity(
+              opacity: 0.1,
+              child: Image.asset(
+                'assets/images/logo_univ.jpeg',
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+          // ListView untuk menampilkan riwayat kehadiran
+          ListView.builder(
+            itemCount: attendanceHistory.length,
+            itemBuilder: (ctx, index) {
+              final record = attendanceHistory[index];
+              return ListTile(
+                title: Text(DateFormat('dd MMM yyyy').format(record['date'])),
+                subtitle: Text('Hadir: ${record['present']}, Tidak Hadir: ${record['absent']}'),
+              );
+            },
+          ),
+        ],
       ),
     );
   }
